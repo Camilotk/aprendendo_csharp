129891369fc098cdc02dbbb56888a75230b03f9c
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2019-08-12 10:15:27 -0300

    Update DatabaseMain.cs

--- dump/DatabaseMain.cs
+++ dump/DatabaseMain.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
 
 namespace TesteEntity
 {
-    class Program
+     public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BloggingContext())
+            {
+
+                // Adiciona um novo Objeto Blog à collection Blogs, isso depois
+                // é mapeado no ORM transformando a Collectione em Tabela e o
+                // Objeto em Input 
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                // Salva as mudanças
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                // Acessa a lista de Blogs e retorna o endereço
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
         }
     }
 }
+
