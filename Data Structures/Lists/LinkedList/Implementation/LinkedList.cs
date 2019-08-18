60df515a432855bb0444c791aa67c9a7c5feac5f
Camilo <camilotk@gmail.com>
2019-08-17 22:05:49 -0300

    update LinkedList.cs

--- Data Structures/Lists/LinkedList/Implementation/LinkedList.cs
+++ Data Structures/Lists/LinkedList/Implementation/LinkedList.cs
@@ -1,14 +1,3 @@
-using System;
-
-// Class Node, where each Node of the List will remain
-public class Node
-{
-    // next object Node stored as Object
-    public Node next;
-    // actual Node data
-    public Object data;
-}
-// -----------------------------------------------------
 // add = O(1) worst case | get = O(n)
 public class LinkedList
 {
@@ -60,32 +49,5 @@ public class LinkedList
 
             current.next = toAdd;
         }
-    }
-}
-// -----------------------------------------------------
-// Main
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Add First:");
-        LinkedList myList1 = new LinkedList();
-
-        myList1.AddFirst("Hello");
-        myList1.AddFirst("Magical");
-        myList1.AddFirst("World");
-        myList1.printAllNodes();
-
-        Console.WriteLine();
-
-        Console.WriteLine("Add Last:");
-        LinkedList myList2 = new LinkedList();
-
-        myList2.AddLast("Hello");
-        myList2.AddLast("Magical");
-        myList2.AddLast("World");
-        myList2.printAllNodes();
-
-        Console.ReadLine();
     }
 }
