76e3c08d7bab5f08f0f1a2d2c15af9bb83ab8bc3
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2019-09-16 18:08:47 -0300

    Update MySortedSet.cs

--- Data Structures/Sets/SortedSet/Implementation/MySortedSet.cs
+++ Data Structures/Sets/SortedSet/Implementation/MySortedSet.cs
@@ -1,1 +1,46 @@
 using System;
+
+namespace SetsTest.SortedSet
+{
+    public class MySortedSet<T> where T : IComparable
+    {
+        private RedBlackTree<T> _elements { get; set; }
+
+        public MySortedSet () => _elements = new RedBlackTree<T>();
+
+        public int Count()
+        {
+            return Convert.ToInt32(_elements.numberOfElements);
+        }
+
+        public bool Add(T item)
+        {
+            try 
+            {
+                _elements.Add(item);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
+        public bool Remove(T item)
+        {
+            try
+            {
+                _elements.Remove(item);
+                return true;
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
